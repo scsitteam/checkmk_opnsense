fa7c8527efcba3eb2463271fd26bf693edf41eb7
Marius Rieder <marius.rieder@scs.ch>
2025-07-22 11:21:22 +0000

    Update copyright

--- rulesets/datasource.py
+++ rulesets/datasource.py
@@ -3,7 +3,7 @@
 #
 # checkmk_opnsense - Checkmk extension for OPNsense
 #
-# Copyright (C) 2024-2025  Marius Rieder <marius.rieder@scs.ch>
+# Copyright (C) 2025-2025  Marius Rieder <marius.rieder@scs.ch>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
